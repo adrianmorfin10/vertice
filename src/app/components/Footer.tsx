a1cd1fb149d40f1333ec012e7fcf39306a04a8ed
adrianmorfin10 <adri_mo95@hotmail.com>
2025-08-27 15:49:45 -0600

    dffffo

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -16,16 +16,16 @@ export const Footer = () => {
             className="mb-4"
           />
           <div className="flex space-x-4">
-            <a href="https://www.facebook.com/mineriavertice" className="text-2xl hover:text-blue-400 transition-colors">
+            <a href="https://www.facebook.com/mineriavertice" className="text-2xl hover:text-yellow-400 transition-colors">
               <FaFacebook />
             </a>
-            <a href="https://x.com/mineriavertice" className="text-2xl hover:text-blue-400 transition-colors">
+            <a href="https://x.com/mineriavertice" className="text-2xl hover:text-yellow-400 transition-colors">
               <FaTwitter />
             </a>
-            <a href="https://www.linkedin.com/company/mineriavertice/" className="text-2xl hover:text-blue-400 transition-colors">
+            <a href="https://www.linkedin.com/company/mineriavertice/" className="text-2xl hover:text-yellow-400 transition-colors">
               <FaLinkedin />
             </a>
-            <a href="https://www.instagram.com/mineriavertice" className="text-2xl hover:text-blue-400 transition-colors">
+            <a href="https://www.instagram.com/mineriavertice" className="text-2xl hover:text-yellow-400 transition-colors">
               <FaInstagram />
             </a>
           </div>
@@ -43,7 +43,7 @@ export const Footer = () => {
           <h3 className="text-xl font-space-grotesk font-light mb-4">Ubicación</h3>
           <p className="font-lato mb-2">Zona Plateada, Pachuca, Hidalgo.</p>
           <p className="font-lato mb-4">CP 42084</p>
-          <a href="ubicaciones" className="flex items-center font-lato hover:text-blue-400 transition-colors">
+          <a href="ubicaciones" className="flex items-center font-lato hover:text-yellow-400 transition-colors">
             <FaMapMarkerAlt className="mr-2" /> Ver en mapa
           </a>
         </div>
@@ -52,10 +52,10 @@ export const Footer = () => {
         <div>
           <h3 className="text-xl font-space-grotesk font-light mb-4">Enlaces</h3>
           <ul className="space-y-2 font-lato">
-            <li><a href="" className="hover:text-blue-400 transition-colors">Inicio</a></li>
-            <li><a href="servicios" className="hover:text-blue-400 transition-colors">Servicios</a></li>
-            <li><a href="inversion" className="hover:text-blue-400 transition-colors">Inversiones</a></li>
-            <li><a href="contacto" className="hover:text-blue-400 transition-colors">Contacto</a></li>
+            <li><a href="" className="hover:text-yellow-400 transition-colors">Inicio</a></li>
+            <li><a href="servicios" className="hover:text-yellow-400 transition-colors">Servicios</a></li>
+            <li><a href="inversion" className="hover:text-yellow-400 transition-colors">Inversiones</a></li>
+            <li><a href="contacto" className="hover:text-yellow-400 transition-colors">Contacto</a></li>
           </ul>
         </div>
       </div>
