48a08dfa0f7fa4aed9a224992d76425ac9486e75
adrianmorfin10 <adri_mo95@hotmail.com>
2026-02-15 23:07:21 -0600

    fuera de servicet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import Image from "next/image";
-import Navbar from "./components/NavBar";
-import Hero from "./components/Hero";
-import UbicacionesHome from "./components/UbicacionesHome";
-import { Servicios } from "./components/Servicios";
-import { Principios } from './components/Principios';
-import ContactForm from './components/ContactForm'
-import { Footer } from './components/Footer'
+
 
 export default function Home() {
   return (
